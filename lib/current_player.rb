de3559745622b9429e2a3c5a41d221145da45186
Sean Xiong <seanxionghe@gmail.com>
2020-04-24 05:17:42 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,15 +1,19 @@
+
+
 def turn_count(board)
   count=0
   board.each do |position|
-  if position == "O" || position == "X"
+    if position == "O" || position == "X"
     count +=1
+  end
   end
   return coount
 end
 
+
+
 def current_player(board)
-  turns = turn_count(board)
-  if turns % 2 == 0
+  turns = turn_count(board) % 2 == 0
     return "X"
   else
     return "O"
